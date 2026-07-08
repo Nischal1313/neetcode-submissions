53cbedddc50d4476876e4fe4e0b0df085a9cfa04
Nischal [Nisse] Gautam <142784199+Nischal1313@users.noreply.github.com>
2026-07-08 19:34:56 +0300

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }        
+        return 1 + max(maxDepth(root->right),maxDepth(root->left));
+        }
+};
+
